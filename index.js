13660ea56682c7212259c20f718173b513624ade
DanieleCapuano <el.einad@gmail.com>
2022-09-14 17:37:38 +0200

    exporting transformations

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { init_scene, run_scene, stop_scene } from "./src/scene";
 import { init_data } from "./src/data";
+import * as Transforms from "./src/transforms";
 
 /**
  * od : Objects Drawer: a simple drawer with WebGL 2
@@ -12,5 +13,6 @@ export {
     init_data,
     init_scene,
     run_scene,
-    stop_scene
+    stop_scene,
+    Transforms
 };
